1f39c78768f8264bb1a709a787802d9e4ef3df6e
Sad Panda <sandpanda@panda.com>
2017-03-04 22:38:52 -0700

    added event source

--- mvc/WebApplication1/react-fronteend/main.js
+++ mvc/WebApplication1/react-fronteend/main.js
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import something from './a'
 
+//TODO: hook up event stream to /messages
 var element = (
   <div>something</div>
 );
 console.log(something.a());
 
+var source = new EventSource('/messages');
 var mountNode = document.getElementById('app');
+
+source.addEventListener('message', function(e) {
+  var node = document.createTextNode(e.data);
+  var br = document.createElement('br');
+  mountNode.appendChild(node);
+  mountNode.appendChild(br);
+}, false);
+
 console.log(mountNode);
 ReactDOM.render(element, mountNode);
