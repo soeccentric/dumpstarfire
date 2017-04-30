ac3dcd6c31146fef95919f4bcff86b0d4d8ef496
Bob Pace <bob.pace@healthmine.com>
2017-04-29 21:15:17 -0600

    fixed issue with gzipped event stream

--- mvc/WebApplication1/Controllers/DefaultController.cs
+++ mvc/WebApplication1/Controllers/DefaultController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
-using System.Reactive;
 using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
@@ -11,64 +9,69 @@ using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
-  public class DefaultController : Controller
-  {
-    private readonly IMessageSource _messageSource;
-    private readonly IEventWriter _eventWriter;
-
-    public DefaultController(IMessageSource messageSource, IEventWriter eventWriter)
+    public class DefaultController : Controller
     {
-      _messageSource = messageSource;
-      _eventWriter = eventWriter;
-    }
+        private readonly IMessageSource _messageSource;
+        private readonly IEventWriter _eventWriter;
 
-    // Maps to http://localhost:61589/
-    //cancelation tokens
-    //react .js front end
-    //facebook fixed data table.
-    [Route]
-    public ViewResult Index()
-    {
-      return View();
-    }
-
-    [Route("messages")]
-    public async Task Messages()
-    {
-      Response.ContentType = "text/event-stream";
-      var queue = new BlockingCollection<string>(); //for producer/consumer pattern
-      //producer adds items to the collection
-      //consumer uses the getconsumingenuerable
-
-      var liveConnection = Observable.Create<string>(
-          o => Response.ClientDisconnectedToken.Register(o.OnCompleted))
-        .Finally(() => queue.CompleteAdding());
-
-      var events = _messageSource.Messages();
-      events.Connect();
-
-      var subscription = events
-        .TakeUntil(liveConnection)
-        .SubscribeOn(TaskPoolScheduler.Default)
-        // if you don't do this it won't be a non blocking subscribe and you will never hit the using code.
-        .Subscribe(serverEvent => queue.Add(serverEvent));
-
-      using (subscription)
-      {
-        var consumer = Task.Run(async () =>
+        public DefaultController(IMessageSource messageSource, IEventWriter eventWriter)
         {
-          foreach (string message in queue.GetConsumingEnumerable()) //forever looping until complete adding is called.
-          {
-            var success = await _eventWriter.WriteEvent(message, Response).ConfigureAwait(false);
-            if (!success)
+            _messageSource = messageSource;
+            _eventWriter = eventWriter;
+        }
+
+        // Maps to http://localhost:61589/
+        //cancelation tokens
+        //react .js front end
+        //facebook fixed data table.
+        [Route]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [Route("messages")]
+        public async Task Messages()
+        {
+            Response.ContentType = "text/event-stream";
+            Response.CacheControl = "no-cache";
+            Response.Headers.Add("Connection", "keep-alive");
+            Response.StatusCode = 200;
+            await Response.FlushAsync();
+
+            var queue = new BlockingCollection<string>(); //for producer/consumer pattern
+            //producer adds items to the collection
+            //consumer uses the getconsumingenuerable
+
+            var liveConnection = Observable.Create<string>(
+                    o => Response.ClientDisconnectedToken.Register(o.OnCompleted))
+                .Finally(() => queue.CompleteAdding());
+
+            var events = _messageSource.Messages();
+            events.Connect();
+
+            var subscription = events
+                .TakeUntil(liveConnection)
+                .SubscribeOn(TaskPoolScheduler.Default)
+                // if you don't do this it won't be a non blocking subscribe and you will never hit the using code.
+                .Subscribe(serverEvent => queue.Add(serverEvent));
+
+            using (subscription)
             {
-              break;
+                var consumer = Task.Run(async () =>
+                {
+                    foreach (string message in queue.GetConsumingEnumerable())
+                        //forever looping until complete adding is called.
+                    {
+                        var success = await _eventWriter.WriteEvent(message, Response).ConfigureAwait(false);
+                        if (!success)
+                        {
+                            break;
+                        }
+                    }
+                });
+                await consumer.ConfigureAwait(false);
             }
-          }
-        });
-        await consumer.ConfigureAwait(false);
-      }
-      Debug.WriteLine("Disconnected");
+        }
     }
-  }
 }
--- mvc/WebApplication1/react-fronteend/main.js
+++ mvc/WebApplication1/react-fronteend/main.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import something from './a'
+import Rx from 'rx-dom'
 
 var element = (
   <div>something</div>
 );
 console.log(something.a());
 
+var observer = Rx.Observer.create(function (e) {
+  console.log('Opening');
+});
+
+var source = Rx.DOM.fromEventSource('/messages', observer)
+source.subscribe((e) => {
+  var node = document.createTextNode(e);
+
+});
+
 var mountNode = document.getElementById('app');
-console.log(mountNode);
-ReactDOM.render(element, mountNode);
+source.subscribe((message) => {
+  var node = document.createTextNode(message);
+  var br = document.createElement('br');
+  mountNode.appendChild(node);
+  mountNode.appendChild(br);
+});
+
+//ReactDOM.render(element, mountNode);
